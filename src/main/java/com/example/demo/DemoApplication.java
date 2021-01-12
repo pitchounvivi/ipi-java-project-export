9c440ee46e37a0d7da3a2e9600f58e4c046fb105
pitchounvivi <pitchounvivi@gmail.com>
2021-01-12 09:30:29 +0100

    Initial commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,5 +11,9 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!");
+        System.out.println("!!DEMARRAGE APPLICATION!!");
+
     }
 }
