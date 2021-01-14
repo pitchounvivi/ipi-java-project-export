3451b40f5cf1e51efbf84dbf1cc3eb5669ad572e
pitchounvivi <pitchounvivi@gmail.com>
2021-01-14 23:59:01 +0100

    Ajout d'éléments pour la facturation dans la base de donnée

--- src/main/java/com/example/demo/service/InitData.java
+++ src/main/java/com/example/demo/service/InitData.java
@@ -2,6 +2,8 @@ package com.example.demo.service;
 
 import com.example.demo.entity.Article;
 import com.example.demo.entity.Client;
+import com.example.demo.entity.Facture;
+import com.example.demo.entity.LigneFacture;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Service;
@@ -38,6 +40,19 @@ public class InitData implements ApplicationListener<ApplicationReadyEvent> {
 
         Client cl1 = createClient("John", "Doe", LocalDate.parse("2000-08-02"));
         Client cl2 = createClient("Jane", "Doe", LocalDate.parse("2000-01-02"));
+
+
+        Facture f1 = createFacture(cl1);
+        Facture f2 = createFacture(cl2);
+        Facture f3 = createFacture(cl1);
+
+
+        LigneFacture lf1 = createLigneFacture(a1, 2, f1);
+        LigneFacture lf2 = createLigneFacture(a2, 1, f1);
+        LigneFacture lf3 = createLigneFacture(a3, 1, f2);
+        LigneFacture Lf4 = createLigneFacture(a2, 3, f3);
+
+
     }
 
     private Client createClient(String prenom, String nom, LocalDate dateNaissance) {
@@ -57,6 +72,23 @@ public class InitData implements ApplicationListener<ApplicationReadyEvent> {
         a1.setDescription(description);
         entityManager.persist(a1);
         return a1;
+    }
+
+
+    private Facture createFacture(Client client){
+        Facture facture = new Facture();
+        facture.setClient(client);
+        entityManager.persist(facture);
+        return facture;
+    }
+
+
+    private LigneFacture createLigneFacture(Article article, int quantite, Facture facture){
+        LigneFacture ligneFacture = new LigneFacture();
+        ligneFacture.setArticle(article);
+        ligneFacture.setQuantite(quantite);
+        entityManager.persist(ligneFacture);
+        return ligneFacture;
     }
 
 }
