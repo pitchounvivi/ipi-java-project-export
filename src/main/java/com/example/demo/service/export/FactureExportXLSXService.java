908f409a49fb1f26de42fad0b141ff08d0a30816
pitchounvivi <pitchounvivi@gmail.com>
2021-01-16 20:02:10 +0100

    Création d'une facture pour un client (base : pas de date, pas de facture détaillée)

--- src/main/java/com/example/demo/service/export/FactureExportXLSXService.java
+++ src/main/java/com/example/demo/service/export/FactureExportXLSXService.java
@@ -4,6 +4,8 @@ import com.example.demo.entity.Client;
 import com.example.demo.repository.ClientRepository;
 import com.example.demo.repository.FactureRepository;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,10 +36,48 @@ public class FactureExportXLSXService {
 
 
             //Créer une feuille vide et son titre
-            Sheet listeFactureClient = wb.createSheet(client.get().getNom());
+            Sheet listeFactureClient = wb.createSheet(client.get().getNom() + client.get().getPrenom());
 
 
-            //Todo
+            //créer une ligne
+            Row rowNom = listeFactureClient.createRow(0);
+            Row rowPrenom = listeFactureClient.createRow(1);
+            Row rowAnnee = listeFactureClient.createRow(2);
+
+
+            //Création de cellule
+            Cell cellNom = rowNom.createCell(0);
+            Cell cellPrenom = rowPrenom.createCell(0);
+            Cell cellAnnee = rowAnnee.createCell(0);
+
+
+            //Mettre leur nom
+            cellNom.setCellValue("Nom :");
+            cellPrenom.setCellValue("Prénom :");
+            cellAnnee.setCellValue("Année de Naissance :");
+
+
+            //Création des cellules pour le client
+            Cell cellNomClient = rowNom.createCell(1);
+            Cell cellPrenomClient = rowPrenom.createCell(1);
+
+
+
+            //Remplissage avec les valeurs client
+            cellNomClient.setCellValue(client.get().getNom());
+            cellPrenomClient.setCellValue(client.get().getPrenom());
+
+
+
+
+
+
+
+            //Forcer la taille automatique des colonnes
+            Sheet sheet = wb.getSheetAt(0);
+            sheet.autoSizeColumn(0); // valable uniquement pour la première colonne
+            sheet.autoSizeColumn(1);
+
 
 
             //écriture du document excel
